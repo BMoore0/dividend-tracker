2eebe7048bcbf5fda4d9adbeee50fa64e8d3109b
BMoore0 <brad.m.3434@gmail.com>
2022-02-27 12:45:39 -0500

    add basic inputs and checks for quarterly/monthly returns

--- src/app/components/stock-item/stock-item.component.ts
+++ src/app/components/stock-item/stock-item.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'stock-item',
   templateUrl: './stock-item.component.html',
   styleUrls: ['./stock-item.component.scss'],
 })
+
 export class StockItemComponent implements OnInit {
+
+  @Input('ticker') ticker = 'AAPL';
+  @Input('shareCount') shareCount = '50';
+  @Input('qtrReturn') qtrReturn = '$130';
+  @Input('mnthReturn') mnthReturn = '';
+  @Input('yrReturn') yrReturn = '$520';
+
+  private monthlyReturnExists;
+  private quarterlyReturnExists;
 
   constructor() { }
 
-  ngOnInit() {}
+  
+  ngOnInit() {
+    this.monthlyReturnExists = this.mnthReturn === '' ?  false : true;
+    this.quarterlyReturnExists = this.qtrReturn === '' ?  false : true;
+    console.log("exists?: ", this.monthlyReturnExists);
+  }
 
 }
