7c20bfe470d4bb7d4e2f758389a65bc219300e0a
BMoore0 <brad.m.3434@gmail.com>
2022-02-27 22:14:40 -0500

    add basic validation

--- src/app/components/add-stock-button/add-stock-button.component.ts
+++ src/app/components/add-stock-button/add-stock-button.component.ts
@@ -12,38 +12,59 @@ export class AddStockButtonComponent implements OnInit {
 
 
   private isModalOpen: boolean;
+  private stockSymbol: string;
+  private shareCount: number;
+  private activateAddButton: boolean;
+
   constructor() {}
 
   ngOnInit() {
     this.isModalOpen = false;
+    this.activateAddButton = false;
+  }
+
+  validateInput() {
+    if(this.stockSymbol && this.shareCount){
+
+      if(this.stockSymbol.length > 0 && (this.shareCount > 0 && this.shareCount < 10000)) {
+        this.activateAddButton = true;
+      }
+      else {
+        this.activateAddButton = false;
+      }
+
+    }
   }
 
   addStockItem () {
-    const info: StockInfo = {
-        ticker: 'TEST',
-        shareCount: 5,
+    this.validateInput();
+
+    if(this.activateAddButton) {
+      const info: StockInfo = {
+        ticker: this.stockSymbol,
+        shareCount: this.shareCount,
         yrReturn: 3,
         qtrReturn: 2,
         mnthReturn: 1
     }
     this.newStockItem.emit(info);
-
-    console.log("stock item: ", info);
-    console.log("Add stock item");
-
+    this.stockSymbol = '';
+    this.shareCount = undefined;
     this.isModalOpen = false;
+    }
+    else {
+      console.log("invalid input please try again");
+    }
   }
 
   openModal() {
-    console.log("open modal, this.isOpen: ", this.isModalOpen);
     this.isModalOpen = true;
-    console.log("open modal, this.isOpen: ", this.isModalOpen);
-
   }
+  
   closeModal() {
-    console.log("close modal");
+    this.stockSymbol = '';
+    this.shareCount = undefined;
     this.isModalOpen = false;
-    console.log(this.isModalOpen);
   }
 
 }
